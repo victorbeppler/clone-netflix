eef733f80faa596c88a787d7b0ec55d06c4f7711
victorbeppler <victorhugobeppler@gmail.com>
2022-03-20 17:48:46 -0300

    build(terminando a criação do destaque)Criado o filme em destaque, reestruturado as pastas dos components, iniciado a criação do header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import tmdb from "./tmdb";
+import MovieRow from "./components/MovieRow";
+import FeaturedMovie from "./components/FeaturedMovie";
+import "./App.css";
+import Header from "./components/Header";
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
-  return <div>Olá Mundo</div>;
+  const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState(null);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await tmdb.getHomeList();
+      setMovieList(list);
+
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await tmdb.getMovieInfo(chosen.id, "tv");
+      setFeatureData(chosenInfo);
+    };
+    loadAll();
+  }, []);
+
+  return (
+    <div className="page">
+      <Header/>
+
+      {featureData && <FeaturedMovie item={featureData} />}
+
+      <section className="listas">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
+  );
 };
